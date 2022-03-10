9d5822b672fb3985bdb0fb7ba9a3929d28aff1b6
Andrew Fleming <66144906+AndrewJFleming@users.noreply.github.com>
2022-03-10 11:47:14 -0800

    Setup two row hybrid Bootstrap navbar with placeholder content.

--- client/src/shared/components/TopNav/TopNav.jsx
+++ client/src/shared/components/TopNav/TopNav.jsx
@@ -1,12 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-import {
-  Container,
-  Navbar,
-  Nav,
-  NavDropdown,
-} from "react-bootstrap";
+import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
 import NavLogo from "../../../images/logoOutline.png";
 import headerBG from "../../../images/headerBGImage.jpg";
 import { categoryData } from "../../../dummyData";
@@ -21,82 +16,66 @@ const TopNav = ({ currentUser }) => {
     setExpanded(false);
   };
 
-  useEffect(() => {
-    setTopNavCats(categoryData);
-  }, []);
+  // useEffect(() => {
+  //   setTopNavCats(categoryData);
+  // }, []);
 
   return (
-    <Navbar expanded={expanded} collapseOnSelect expand="lg" variant="dark" style={{
+    <Navbar
+      expanded={expanded}
+      collapseOnSelect
+      expand="lg"
+      variant="dark"
+      style={{
         background: `url(${headerBG})`,
-      }}>
-      <Container>
-        <Navbar.Brand as={Link} to="/" onClick={handleCollapse}>
+      }}
+    >
+      <Container className="d-flex justify-content-between mobile-container">
+        <Link to="#">
           <img
             src={NavLogo}
-            height="30"
+            height="50"
             className="d-inline-block align-top"
-            alt="site logo"
+            alt="site logo mobile"
           />
-          <span className="pl-1">Purgation</span>
-        </Navbar.Brand>
+        </Link>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
           onClick={() => setExpanded(expanded ? false : "expanded")}
         />
-        <Navbar.Collapse id="responsive-navbar-nav" className="myResponsive">
-          <Nav>
-            <NavDropdown title="Categories" id="basic-nav-dropdown">
-              {topNavCats?.map((cat) => (
-                <NavDropdown.Item
-                  key={`${cat.title}-navItem`}
-                  as={Link}
-                  to={`/product-list/${cat.title}`}
-                  onClick={handleCollapse}
-                >
-                  {cat.title}
-                </NavDropdown.Item>
-              ))}
-            </NavDropdown>
-          </Nav>
-          {/* <Nav>
-            {currentUser ? (
-              <Nav.Link as={Link} to="/" onClick={handleLogout}>
-                Logout
-              </Nav.Link>
-            ) : (
-              <React.Fragment>
-                <Nav.Link as={Link} to="/login" onClick={handleCollapse}>
-                  Login
-                </Nav.Link>
-                <Nav.Link as={Link} to="/register" onClick={handleCollapse}>
-                  Register
-                </Nav.Link>
-              </React.Fragment>
-            )}
-          </Nav> */}
-          <Nav>
-            {/* {currentUser && (
-              <Link
-                to={`account/${currentUser._id}`}
-                className="avatar-link nav-link"
-              >
-                <span className="avatar">{currentUser.username}</span>
-              </Link>
-            )} */}
-            <Nav.Link
-              className="nav-cart-link nav-link"
-              as={Link}
-              to="/cart"
-              onClick={handleCollapse}
-            >
-              <span className={`${cartCount && "positive-cart"}`}>
-                <i className="fas fa-shopping-basket"></i>
-                &nbsp;({cartCount})
-              </span>
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
       </Container>
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Container className="d-flex justify-content-between">
+          <div className="desktop-nav-logo">
+            <Link to="#">
+              <img
+                src={NavLogo}
+                height="30"
+                className="d-inline-block align-top"
+                alt="site logo"
+              />
+              <span className="pl-1">Purgation</span>
+            </Link>
+          </div>
+          <div className="nav-items-top">
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </div>
+          <div>
+            <input type="text" id="nav-search" name="search" />
+          </div>
+        </Container>
+        <Container className="d-flex justify-content-between">
+          <div className="nav-items-bottom">
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </div>
+          <div>
+            <i className="fas fa-shopping-basket nav-cart-icon"></i>
+          </div>
+        </Container>
+      </Navbar.Collapse>
+      {/* </nav> */}
     </Navbar>
   );
 };
